0a847a81febd05acae777811465a237568b8f6bc
CRISTIAN CAETANO DA SILVA <u21724@LAPA12.academico.cotuca.unicamp.br>
2022-09-21 16:52:43 -0300

    Fix error

--- src/components/Cursos/cursos.js
+++ src/components/Cursos/cursos.js
@@ -1,33 +1,55 @@
 import React from "react";
 import './cursos.css'
 
-export default Cursos = () => {
+const Cursos = () => {
     return (
-        <div className="Container">
+        <div className="inclui-container">
+            <label> RA: </label>
+            <input
+                type="text"
+                id="ra"
+                placeholder="RA do aluno"
+                className="form-input"
+                name="ra"
+                
+                /*value={this.state.aluno.ra}
+                onChange={e => this.atualizaCampo(e)}*/
+                
+            />
+            <label> Nome: </label>
+            <input
+                type="text"
+                id="nome"
+                placeholder="Nome do aluno"
+                className="form-input"
+                name="nome"
+
+                /*value={this.state.aluno.nome}
+                onChange={e => this.atualizaCampo(e)}*/
+            />
             <label> Código do Curso: </label>
-                <input
-                    type="text"
-                    id="ra"
-                    placeholder="RA do aluno"
-                    className="form-input"
-                    name="ra"
-                />
-                <label> nome do Curso: </label>
-                <input
-                    type="text"
-                    id="ra"
-                    placeholder="RA do aluno"
-                    className="form-input"
-                    name="ra"
-                />
-                <label> Periodo: </label>
-                <input
-                    type="text"
-                    id="ra"
-                    placeholder="RA do aluno"
-                    className="form-input"
-                    name="ra"
-                />
+            <input
+                type="number"
+                id="codCurso"
+                placeholder="0"
+                className="form-input"
+                name="codCurso"
+
+                /*value={this.state.aluno.codCurso}
+                onChange={e => this.atualizaCampo(e)}*/
+            />
+            <button className="btnSalvar"
+                /*onClick={e => this.salvar(e)}*/ >
+                Salvar
+            </button>
+            <button className="btnCancelar"
+                /*onClick={e => this.limpar(e)}*/ >
+                Cancelar
+            </button>
         </div>
+
+        
     )
 }
+
+export default Cursos
